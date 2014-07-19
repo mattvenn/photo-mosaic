d6b08ffe979d4238e546cba329af80c9a7bd6f96
matt venn <matt@mattvenn.net>
2014-07-19 23:01:18 +0100

    creates lots of coloured square images

---
+++ image_creator.py
@@ -0,0 +1,10 @@
+from PIL import Image, ImageDraw
+size = 50
+colour_step = 50
+for R in range(0,255,colour_step):
+    for G in range(0,255,colour_step):
+        for B in range(0,255,colour_step):
+            image = Image.new('RGB',(size,size), (R,G,B))
+            filename = "%03d%03d%03d.jpg" % (R,G,B)
+            image.save(filename)
+
---
+++ montager.py
@@ -0,0 +1,42 @@
+from PIL import Image, ImageDraw
+import os
+import random
+
+image_dir = 'images/'
+
+#how big we want the montage to be
+mont_width = 500
+mont_height = 500
+
+#number of tiles
+tiles = 10
+
+#sums to work out how many and how big the tiles are
+x_tiles = mont_width / tiles
+y_tiles = mont_height / tiles
+tile_width = mont_width / tiles
+
+#create a blank image
+montage_image = Image.new('RGB',(mont_width,mont_height), "white")
+
+#get all the images in our image directory
+all_files = os.listdir(image_dir)
+num_files = len(all_files)
+
+#now for all the tiles in the image
+for x in range(0,mont_width,tile_width):
+    for y in range(0,mont_width,tile_width):
+
+        #pick a random image from the directory
+        file = all_files[random.randint(0,num_files-1)]
+        tile = Image.open(image_dir + file)
+
+        #thumbnail it
+        tile.thumbnail((tile_width,tile_width))
+
+        #and paste it in
+        box=(x,y,x+tile_width,y+tile_width)
+        montage_image.paste(tile,box)
+
+#save it!
+montage_image.save("montage.jpg")
